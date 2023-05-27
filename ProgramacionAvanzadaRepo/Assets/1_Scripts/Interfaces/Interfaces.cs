db34ec7b04944094793f7c0f6a0b4c84e2eec7c0
str3ng1 <80906043+str3ng1@users.noreply.github.com>
2023-05-27 15:54:32 +0200

    Interfaces

---
+++ ProgramacionAvanzadaRepo/Assets/1_Scripts/Interfaces/Interfaces.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Interfaces 
+{
+    public interface IBuyable
+    {
+        public void Buy() { }
+        public int GetPrice();
+    }
+
+    public interface ISellable
+    {
+        public void Sell() { }
+        public int GetSellingPrice();  
+    }
+
+    public interface IDamageable
+    {
+        public void TakeDamage(int amount);
+        public void Die();
+    }
+
+    public interface IInteractable
+    {
+        public void Interact();
+        bool canInteractAgain { get { return canInteractAgain; } set { canInteractAgain = value; } }
+    }
+
+}
